aa281a6fed8ad29bb47f6d7469924fcb0f0d0158
Pheonix Jones <codermonkeyjs@gmail.com>
2016-10-23 13:16:06 -0600

    Spark Java put in Main method

--- src/main/java/com/codermonkeyjs/rest/Main.java
+++ src/main/java/com/codermonkeyjs/rest/Main.java
@@ -1,7 +1,11 @@
 package com.codermonkeyjs.rest;
+import static spark.Spark.*;
 
 /**
  * Created by Pheonix on 10/22/2016.
  */
 public class Main {
+    public static void main (String[] args){
+        get("/hello",(req, res) -> "Hello World");
+    }
 }
